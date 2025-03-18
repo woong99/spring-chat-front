17ebcecc4686cef77c6fa67a21e4391a4e7f0367
woong99 <dndry1072@naver.com>
2025-03-18 14:34:49 +0900

    feat: eslint 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,49 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+  root: true,
+  extends: ['plugin:tailwindcss/recommended'],
+  plugins: ['unused-imports', 'import'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    tailwindcss: {
+      callees: ['cn'],
     },
   },
-  plugins: ['react', 'react-hooks', 'jsx-a11y'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': 'warn',
-  },
-  settings: {
-    react: { version: 'detect' },
+    // https://github.com/sweepline/eslint-plugin-unused-imports
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/prefer-default-export': 'off',
+    'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
+    // https://nextjs.org/docs/basic-features/eslint
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
