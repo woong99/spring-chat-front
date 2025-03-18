af8da5cf2142558ed4a68b15a290e22ee52cb4f7
woong99 <dndry1072@naver.com>
2025-03-18 14:37:20 +0900

    feat: react-modal, react-query 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import 'react-loading-skeleton/dist/skeleton.css';
+import 'react-toastify/dist/ReactToastify.css';
+import Modal from 'react-modal';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(<App />);
+Modal.setAppElement('#root');
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
