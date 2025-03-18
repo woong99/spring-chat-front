350f1d8f81873908052a9123d71b7b8671d4280a
woong99 <dndry1072@naver.com>
2025-03-18 14:37:49 +0900

    feat: 네비게이션 메뉴 추가
    1. 친구찾기/친구/채팅/전체채팅 으로 메뉴 추가 및 변경

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,4 +1,4 @@
-import { FaComments, FaUserFriends } from 'react-icons/fa';
+import { FaComment, FaComments, FaUser, FaUserFriends } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
@@ -6,7 +6,7 @@ const Navigation = () => {
     <nav className='bg-white border-t h-16'>
       <div className='flex justify-around h-full'>
         <NavLink
-          to='/'
+          to='/find-friend'
           className={({ isActive }) =>
             `flex flex-col items-center py-3 px-5 ${
               isActive ? 'text-indigo-600' : 'text-gray-500'
@@ -14,7 +14,18 @@ const Navigation = () => {
           }
         >
           <FaUserFriends size={24} />
-          <span className='text-xs mt-1'>전체 채팅방</span>
+          <span className='text-xs mt-1'>친구찾기</span>
+        </NavLink>
+        <NavLink
+          to='/my-friends'
+          className={({ isActive }) =>
+            `flex flex-col items-center py-3 px-5 ${
+              isActive ? 'text-indigo-600' : 'text-gray-500'
+            }`
+          }
+        >
+          <FaUser size={24} />
+          <span className='text-xs mt-1'>친구</span>
         </NavLink>
         <NavLink
           to='/my-chats'
@@ -24,8 +35,19 @@ const Navigation = () => {
             }`
           }
         >
+          <FaComment size={24} />
+          <span className='text-xs mt-1'>채팅</span>
+        </NavLink>
+        <NavLink
+          to='/all-chats'
+          className={({ isActive }) =>
+            `flex flex-col items-center py-3 px-5 ${
+              isActive ? 'text-indigo-600' : 'text-gray-500'
+            }`
+          }
+        >
           <FaComments size={24} />
-          <span className='text-xs mt-1'>내 채팅방</span>
+          <span className='text-xs mt-1'>전체채팅</span>
         </NavLink>
       </div>
     </nav>
